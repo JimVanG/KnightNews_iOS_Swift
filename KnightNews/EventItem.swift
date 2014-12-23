edfb628e08e90577c5a8647293aa1fe287679124
JimVanG <jjvg@me.com>
2014-12-22 20:21:26 -0500

    XML parsing complete in the EventsVC

--- KnightNews/EventItem.swift
+++ KnightNews/EventItem.swift
@@ -9,31 +9,9 @@
 import Foundation
 
 class EventItem: NSObject {
-    var eventTitle: String {
-        get{
-            return self.eventTitle
-        }
-        set{
-            self.eventTitle = newValue
-        }
-    }
-    var eventDesc: String {
-        get{
-            return self.eventDesc
-        }
-        set{
-            self.eventDesc = newValue
-        }
-    }
-    var eventDate: String {
-        get{
-            return self.eventDate
-        }
-        set{
-            self.eventDate = newValue
-        }
-    }
-    
+    var eventTitle = String()
+    var eventDesc = String()
+    var eventDate = String()
     
     
 
--- KnightNews/EventsViewController.swift
+++ KnightNews/EventsViewController.swift
@@ -20,23 +20,91 @@ class EventsViewController: UITableViewController, NSXMLParserDelegate {
     var tempDesc: String = ""
     var elementString: String = ""
     var items = [EventItem]()
+    var eventItem = EventItem()
     
     required init(coder aDecoder: NSCoder) {
-        xmlParser = NSXMLParser()
+        self.xmlParser = NSXMLParser(contentsOfURL: NSURL(string: "http://knightnews.com/events.xml"))!
         super.init(coder: aDecoder)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.xmlParser.delegate = self
+        self.xmlParser.shouldResolveExternalEntities = false
+        
+        self.xmlParser.parse()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: NSXMLParser Delegate Methods
+    
+    func parser(parser: NSXMLParser!, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: [NSObject : AnyObject]!) {
+        
+        self.elementString = elementName
+        
+        if self.elementString == EVENT_NAME {
+            self.eventItem = EventItem()
+            
+            self.tempName = ""
+            self.tempDesc = ""
+            self.tempDate = ""
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, foundCharacters string: String!) {
+        
+        if self.elementString == EVENT_NAME {
+            self.tempName += string
+        }
+        else if self.elementString == EVENT_DATE {
+            self.tempDate += string
+            
+        }else if self.elementString == EVENT_DESC {
+            self.tempDesc += string
+        }
+    }
+    
+    func parser(parser: NSXMLParser!, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+        
+        if self.elementString == EVENT_NAME {
+            self.eventItem.eventTitle = self.tempName
+        }
+        else if self.elementString == EVENT_DATE {
+            self.eventItem.eventDate = self.tempDate
+            
+        }else if self.elementString == EVENT_DESC {
+            self.eventItem.eventDesc = self.tempDesc
+            
+            self.items.append( self.eventItem)
+        }
+    }
 
+    func parserDidEndDocument(parser: NSXMLParser!) {
+        self.tableView.reloadData()
+        
+        println(self.items)
+    }
     
 
+    // MARK: UITableView Methods
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.items.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("EventsTableViewCell", forIndexPath: indexPath) as UITableViewCell
+        
+        
+        
+        return cell
+    }
 }
 
