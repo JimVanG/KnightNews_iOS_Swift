a126965cef00bab57d2299867c36e719eb1398b3
JimVanG <jjvg@me.com>
2014-12-23 10:59:15 -0500

    Added blank WebViewController class

---
+++ KnightNews/WebViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WebViewController.swift
+//  KnightNews
+//
+//  Created by james van gaasbeck on 12/23/14.
+//  Copyright (c) 2014 james van gaasbeck. All rights reserved.
+//
+
+import Foundation
